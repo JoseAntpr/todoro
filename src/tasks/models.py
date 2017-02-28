f55784eaf72c19ad173196aa00e32f249da17955
Jose Antonio Palacios Ramírez <josepio11.uma@gmail.com>
2017-03-01 00:17:22 +0100

    my first commit

---
+++ src/tasks/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+
+    PENDING = "PEN"
+    DONE = "DON"
+    STATUSES = (
+        (PENDING, "Pending"),
+        (DONE,"Done")
+    )
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=3, default=PENDING, choices=STATUSES)
+    time_estimated = models.IntegerField(null=True)
+    deadline = models.DateField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True) # Automaticamente se añade la fecha de creación.
+    modified_at = models.DateTimeField(auto_now=True) # Automáticamente se actualiza la fecha al guardar.
+
+    def __str__(self):
+        return self.name
